88eedfe144c6d662bbedbce53a1d8bb548c11f83
Chien-Yu Chen <austin110068@gmail.com>
2021-07-27 23:59:52 -0700

    Implement proceed to checkout page

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Cart from '../views/Cart.vue';
 import SignUp from '../views/SignUp.vue';
 import LogIn from '../views/LogIn.vue';
 import MyAccount from '../views/MyAccount.vue';
+import Checkout from '../views/Checkout.vue'
 
 const routes = [
   {
@@ -51,6 +52,14 @@ const routes = [
     path: '/cart',
     name: 'Cart',
     component: Cart
+  },
+  {
+    path: '/cart/checkout',
+    name: 'Checkout',
+    component: Checkout,
+    meta: {
+        requireLogin: true
+    }
   },
   {
     path: '/:category_slug/:product_slug',
